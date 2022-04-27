0200fd195e493b2c462d4a5c27138f8cbd8e3ca1
Tanjid Khan Arnob <tanjidkhanarnob2000@gmail.com>
2022-04-28 02:19:08 +0600

    Added Three Cards

--- client/src/Components/Layout/ModalForm.jsx
+++ client/src/Components/Layout/ModalForm.jsx
@@ -6,55 +6,60 @@ import react from "react";
 import reactDOM from "react-dom";
 import styles from "./ModalForm.module.css";
 
-const Backdrop = ({setOpenModal}) => {
-  return <div className={styles.backdrop} onClick={()=>setOpenModal(false)}></div>;
+const Backdrop = ({ setOpenModal }) => {
+  return (
+    <div className={styles.backdrop} onClick={() => setOpenModal(false)}></div>
+  );
 };
-const Overlay = ({setOpenModal}) => {
-  
+const Overlay = ({ setOpenModal }) => {
   return (
     <div className={styles.container}>
-    <Card className={styles.card}>
-      <CardContent>
-        <form className={styles.form}>
-          <div className={styles.infos}>
-            <label>Name:</label>
-            <input type="text"></input>
-            <label>E-mail:</label>
-            <input type="e-mail"></input>
-            <label>Phone:</label>
-            <input type="number"></input>
-          </div>
-          <div className={styles.times}>
-            <label>Time:</label>
-            <input type="time"></input>
-            <label>Date:</label>
-            <input type="date"></input>
-            <label>Medical History:</label>
-            <input type="text"></input>
-          </div>
-        </form>
-      </CardContent>
-      <CardActions>
-        <Button className={styles.button1} onClick={()=>setOpenModal(false)}>
-          Submit
-        </Button>
-      </CardActions>
-    </Card>
+      <Card className={styles.card}>
+        <h2 className={styles.h2}>Give Your Information Here</h2>
+        <CardContent>
+          <form className={styles.form}>
+            <div className={styles.infos}>
+              <label>Name:</label>
+              <input type="text"></input>
+              <label>E-mail:</label>
+              <input type="e-mail"></input>
+              <label>Phone:</label>
+              <input type="number"></input>
+            </div>
+            <div className={styles.times}>
+              <label>Time:</label>
+              <input type="time"></input>
+              <label>Date:</label>
+              <input type="date"></input>
+              <label>Medical History:</label>
+              <input type="text"></input>
+            </div>
+          </form>
+        </CardContent>
+        <CardActions>
+          <Button
+            className={styles.button1}
+            onClick={() => setOpenModal(false)}
+          >
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
 
 const portalElement = document.querySelector("#overlay");
 
-
-const ModalForm = ({setOpenModal}) => {
+const ModalForm = ({ setOpenModal }) => {
   return (
     <react.Fragment>
-       <Overlay setOpenModal={setOpenModal}/>
-       
-      {reactDOM.createPortal(<Backdrop setOpenModal={setOpenModal}/>,portalElement)}
-      
-     
+      <Overlay setOpenModal={setOpenModal} />
+
+      {reactDOM.createPortal(
+        <Backdrop setOpenModal={setOpenModal} />,
+        portalElement
+      )}
     </react.Fragment>
   );
 };
