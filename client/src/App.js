f21ccd025892f221945011c4daff4c31789793c0
Tanjid Khan Arnob <tanjidkhanarnob2000@gmail.com>
2022-04-28 00:28:06 +0600

    Added Modal

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Slider  from './UI Components/Slider/Slider';
-import { style } from '@mui/system';
+import {useState} from 'react'
+import ModalForm from './Components/Layout/ModalForm';
+
+
+
+
 
 
 function App() {
+  const [openModal,setOpenModal]=useState(false);
+  const willOpenModal = (props)=>{
+    setOpenModal(true);
+  }
+  // const willCloseModal = (props)=>{
+  //   setOpenModal(false);
+  // }
   return (
     <div className="App">
       <Navbar/>
       <Slider/>
+      <button onClick={willOpenModal}>Open Modal</button>
+      {openModal && <ModalForm setOpenModal={setOpenModal}/>}
       
       <Footer/>
     </div>
